85b55da57f4632f13515c7e8c392bd7159557214
Evan Barker <evanbarker@Evans-MacBook-Air.local>
2024-09-29 22:49:36 -0600

    changes made

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ app.options('*', cors());
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
