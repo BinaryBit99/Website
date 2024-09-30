78fafce4b95a81d50180b49f19f5da9c20665d00
Evan Barker <evanbarker@Evans-MacBook-Air.local>
2024-09-30 11:57:46 -0600

    fix CORS

--- server.js
+++ server.js
@@ -2,16 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path'); // Add this for path handling
+const path = require('path'); // For path handling
 
 const app = express();
 const mongoURI = process.env.MONGO_URI; // Use the environment variable
 const PORT = process.env.PORT || 3000; // Using port 3000 for the backend
 
 // Middleware for CORS
+const allowedOrigins = [
+    'https://barkerportfolio.ca', 
+    'https://www.barkerportfolio.ca'
+];
+
 app.use(cors({
-    origin: ['https://barkerportfolio-42ba600f168b.herokuapp.com/'], // Add your front-end URL here
-    methods: ['GET', 'POST', 'OPTIONS'], // Ensure OPTIONS is included
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps or Postman)
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy does not allow access from this origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'OPTIONS'], // Ensure OPTIONS is included for preflight requests
     credentials: true // Allow credentials (cookies, authorization headers, etc.)
 }));
 
@@ -21,6 +33,14 @@ app.use(bodyParser.json());
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+// Enforce HTTPS
+app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+});
+
 // Route to serve the main BarkerPortfolio.html file when accessing '/'
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'BarkerPortfolio.html'));
@@ -28,8 +48,8 @@ app.get('/', (req, res) => {
 
 // MongoDB connection
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Define a schema and model for storing contact form submissions
 const contactSchema = new mongoose.Schema({
@@ -65,4 +85,5 @@ app.options('*', cors());
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 
